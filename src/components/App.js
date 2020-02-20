ccf4feb1185d716eadb045809c35b28304b0d6d5
Sharvanand <thirusharwan@gmail.com>
2020-02-19 23:10:31 -0500

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PostList from "./PostList";
+
+function App() {
+  return (
+    <div className="container">
+      <h4 className="center grey-text">Posts App</h4>
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
